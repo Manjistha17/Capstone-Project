2a19a08fd77599983ce72bce500591e82381f65d
Manjistha17 <mmanjistha@gmail.com>
2023-10-15 12:51:49 +0530

    Update depoyment.sh

--- depoyment.sh
+++ depoyment.sh
@@ -1,21 +1,10 @@
 # Tag and upload an image to Docker Hub
 
-# Set vars
-DOCKER_HUB_ID="manjistha"
-DOCKER_REPOSITORY="project5"
-DEPLOYMENT_NAME=${DOCKER_REPOSITORY}
-CONTAINER_PORT=80
-VERSION=1.000
-REPLICAS=4
-
-dockerpath=${DOCKER_HUB_ID}/${DOCKER_REPOSITORY}:${VERSION}
-
 # Run the Docker Hub container with kubernetes
-kubectl create deployment ${DEPLOYMENT_NAME} --image=${dockerpath} --replicas=${REPLICAS} &&
-    kubectl expose deployment/${DEPLOYMENT_NAME} --type="LoadBalancer" --port ${CONTAINER_PORT}
+kubectl create deployment project5 --image=manjistha/project5 --replicas=4 &&
+    kubectl expose deployment/project5 --type="LoadBalancer" --port 80
 
 # List kubernetes pods
 kubectl get pods -o wide
-
-# Forward the container port to a host port
-kubectl port-forward service/${DEPLOYMENT_NAME} ${HOST_PORT}:${CONTAINER_PORT}
+kubectl get services -o wide
+kubectl get pods -o wide
